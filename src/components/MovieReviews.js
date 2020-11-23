31306b822386aa6fb5e0dac380ae1791958274a0
I-Am-Lain <vetter.william@gmail.com>
2020-11-23 11:17:19 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+
+// rafce
+
+import React from 'react'
+
+const MovieReviews = (props) => {
+
+    return (
+        <div className='review-list'>
+            {
+            props.reviews.map(review => {
+                return (
+                    <ul key={review.link.url} className='review'>
+                        <li><a href={review.link.url}> {review.headline} </a></li>
+                        <li>{review.byline}</li>
+                    </ul>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(resp => resp.json())
+        .then(json => this.setState({reviews: json.results}))
+    }
+
+    render() {
+        return (
+            <div style={{border: '2px black solid'}} className='searchable-movie-reviews'>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' name='searchTerm' value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <input type='submit' value='Search'/>
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
